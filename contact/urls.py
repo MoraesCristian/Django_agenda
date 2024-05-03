6ebb2fd57e2c23be205d2dd5ce09369be120f829
Cristian Moraes <cristian.hennequin1@gmail.com>
2024-05-02 23:06:48 -0300

    Aula 460 ate 470

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,8 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact2, name= 'contact2' ),
+    path('search/', views.search, name= 'search' ),
     path('', views.index, name= 'index' ),
+    
 ]
