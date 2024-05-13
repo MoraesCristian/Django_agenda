c62eafdacb0532583ccdb7202db44e30a9ee36b6
Cristian Moraes <cristian.hennequin1@gmail.com>
2024-05-13 18:48:54 -0300

    aulas 478

--- contact/urls.py
+++ contact/urls.py
@@ -5,8 +5,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact2, name= 'contact2' ),
-    path('search/', views.search, name= 'search' ),
     path('', views.index, name= 'index' ),
+    path('search/', views.search, name= 'search' ),
+# CRUD
+    path('contact/<int:contact_id>/detail/', views.contact2, name= 'contact2' ),
+    path('contact/create/', views.create, name= 'create' ),
+    # path('contact/<int:contact_id>/update/', views.contact2, name= 'contact2' ),
+    # path('contact/<int:contact_id>/delete/', views.contact2, name= 'contact2' ),
     
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
 
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
@@ -9,11 +10,15 @@ from contact.models import Contact
 def index(request):
     contact = Contact.objects \
         .filter(show=True)\
-        .order_by("-id")[:10]\
+        .order_by("-id")\
+    
+    paginator = Paginator(contact, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
         
     # print(contact.query) # MOSTRA A QUERY SENDO UTILIZADA
     context = {
-        'contacts':contact,
+        'page_obj':page_obj,
         'site_title': 'Contatos - '
     }
     
@@ -39,11 +44,16 @@ def search(request):
             Q(email__icontains=search_value) 
             )\
         .order_by("-id")
+    
+    paginator = Paginator(contact, 10)  # Show 25 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
         
     # print(contact.query) # MOSTRA A QUERY SENDO UTILIZADA
     context = {
-        'contacts':contact,
-        'site_title': 'Contatos - '
+        'page_obj':page_obj,
+        'site_title': 'Contatos - ',
+        'search_value': search_value,
     }
     
     return render(
