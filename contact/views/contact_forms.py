547492da26700ab282d9b2b04c76771565f0e98f
Cristian Moraes <cristian.hennequin1@gmail.com>
2024-06-03 16:10:43 -0300

    Aula 497 -- iniciando deploy

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,50 +1,93 @@
-
-from django import forms
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
-from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
+from contact.forms import ContactForm
 from contact.models import Contact
 
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-                  )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        
-        self.add_error(
-            'first_name', 
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        return super().clean()
-
-
-def create(request):    
-    if request.method == 'POST':    
+@login_required(login_url='contact:login')
+def create(request):
+    form_action = reverse('contact:create')
+    if request.method == 'POST':
+        form =ContactForm(request.POST,request.FILES)
         context = {
-            'form':ContactForm(request.POST)
+            'form': form,
+            'form_action': form_action,
         }
         
+        if form.is_valid():
+            contact = form.save(commit=False) # Passando como false, voce pode fazer alterações dentro do contato antes de ser salvo ao passar para o banco
+            contact.owner = request.user
+            contact.save()
+            
+            return redirect('contact:update', contact_id= contact.pk)
+            
         return render(
             request,
             'contact/create.html',
             context  
         )
+        
     context = {
-    'form' :ContactForm()
+        'form' :ContactForm(),
+        'form_action': form_action,
     }
     
     return render(
         request,
         'contact/create.html',
         context 
+    )
+
+@login_required(login_url='contact:login')
+def update(request,contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True,owner=request.user)
+    
+    form_action = reverse('contact:update', args=(contact_id,))
+    
+    if request.method == 'POST':
+        form =ContactForm(request.POST, request.FILES, instance=contact)
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+        
+        if form.is_valid():
+            contact = form.save() # form.save(commit=False) -> Passando como false, voce pode fazer alterações dentro do contato antes de ser salvo ao passar para o banco
+            return redirect('contact:update', contact_id=contact.pk)
+            
+        return render(
+            request,
+            'contact/create.html',
+            context  
+        )
+    context = {
+        'form' : ContactForm(instance=contact),
+        'form_action' : form_action,
+    }
+    
+    return render(
+        request,
+        'contact/create.html',
+        context 
+    )
+
+@login_required(login_url='contact:login')
+def delete(request,contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True,owner=request.user)
+
+    confirmation = request.POST.get('confirmation', 'no')
+        
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+    
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation':confirmation,
+        }
     )
